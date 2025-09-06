865987645ce22b365958ded92c8f1c106ce81de2
Leonardo Leodev <lleodev@c2r11s9.42luanda.com>
2025-09-06 17:32:21 +0100

    notification filter done

--- src/components/CustomComponents/NotificationView.tsx
+++ src/components/CustomComponents/NotificationView.tsx
@@ -7,41 +7,59 @@ const NotificationView: React.FC<any> = () => {
 
     const { theme } = useTheme();
 
-    const [ selected, setSelected ] = useState<'none' | 'alerts' | 'Menssages' | 'no-read'>('none')
+    const [ selected, setSelected ] = useState<'none' | 'alerts' | 'Menssages' | 'no-read' | 'all'>('all')
+    const [ filter, setFilter ] = React.useState<any>([])
+    const [ notifications, setNotifications ] = React.useState([
+            {
+                type: 'info',
+                title: 'Atualização disponível',
+                content: 'Uma nova versão do aplicativo está pronta para download.',
+                timestamp: '2025-09-06T09:00:00Z',
+                read: true
+            },
+            {
+                type: 'warning',
+                title: 'Uso de armazenamento alto',
+                content: 'Você está utilizando 90% do seu armazenamento.',
+                timestamp: '2025-09-05T17:45:00Z',
+                read: false
+            },
+            {
+                type: 'success',
+                title: 'Backup concluído',
+                content: 'Seu backup foi finalizado com sucesso.',
+                timestamp: '2025-09-05T12:30:00Z',
+                read: true
+            },
+            {
+                type: 'error',
+                title: 'Erro de conexão',
+                content: 'Não foi possível se conectar ao servidor. Tente novamente.',
+                timestamp: '2025-09-04T22:15:00Z',
+                read: false
+            },
+            {
+                type: 'info',
+                title: 'Nova mensagem',
+                content: 'Você recebeu uma nova mensagem de João.',
+                timestamp: '2025-09-04T08:10:00Z',
+                read: false
+            }
+        ])
+    
+    React.useEffect(() => {
+        
+        console.log(selected)
+        setFilter(() => {
 
-    const notifications = [
-        {
-            type: 'info',
-            title: 'Atualização disponível',
-            content: 'Uma nova versão do aplicativo está pronta para download.',
-            timestamp: '2025-09-06T09:00:00Z'
-        },
-        {
-            type: 'warning',
-            title: 'Uso de armazenamento alto',
-            content: 'Você está utilizando 90% do seu armazenamento.',
-            timestamp: '2025-09-05T17:45:00Z'
-        },
-        {
-            type: 'success',
-            title: 'Backup concluído',
-            content: 'Seu backup foi finalizado com sucesso.',
-            timestamp: '2025-09-05T12:30:00Z'
-        },
-        {
-            type: 'error',
-            title: 'Erro de conexão',
-            content: 'Não foi possível se conectar ao servidor. Tente novamente.',
-            timestamp: '2025-09-04T22:15:00Z'
-        },
-        {
-            type: 'info',
-            title: 'Nova mensagem',
-            content: 'Você recebeu uma nova mensagem de João.',
-            timestamp: '2025-09-04T08:10:00Z'
-        }
-    ];
-
+            if (selected == 'no-read')
+                return notifications.filter((item, index) => (!item.read) )
+            else
+                return(notifications)
+        })
+        
+        console.log(filter)
+    }, [selected])
 
     return (
         <CustomDiv 
@@ -54,14 +72,14 @@ const NotificationView: React.FC<any> = () => {
                 </CustomDiv>
                 <CustomDiv className="flex-box flex-box-column" style={{  gap: '15px' }}>
                     <CustomDiv className="flex-box flex-box-row" style={{ fontSize: '14px', gap: '10px' } }>
+                        <button onClick={() => setSelected("all") } className={"not-filter-btn " + ( selected == 'all' ? ( theme == 'dark' ? 'btn-selected-dark': 'btn-selected-light' ) : '' ) }>Todas</button>
                         <button onClick={() => setSelected("no-read")} className={"not-filter-btn " + ( selected == 'no-read' ? ( theme == 'dark' ? 'btn-selected-dark': 'btn-selected-light' ) : '' ) }>Nao lida</button>
                         <button onClick={() => setSelected('alerts')} className={"not-filter-btn " + ( selected == 'alerts' ? ( theme == 'dark' ? 'btn-selected-dark': 'btn-selected-light' ) : '' ) }>Alertas</button>
                         <button onClick={() => setSelected('Menssages')} className={"not-filter-btn " +  ( selected == 'Menssages' ? ( theme == 'dark' ? 'btn-selected-dark': 'btn-selected-light' ) : '' ) }>Mesagens</button>
                     </CustomDiv>
                     <div style={{overflowY: 'scroll', height: '300px'}}>
-
                    {
-                        notifications.map((item, index) => ( <Notify notify={item} key={index} /> ))
+                        filter && filter.length > 0 ? filter.map((item: any, index) => ( <Notify notify={item} key={index} /> )) : null
                    }
                     </div>
                 </CustomDiv>
@@ -70,16 +88,24 @@ const NotificationView: React.FC<any> = () => {
     )
 }
 
-type NotifyProp = { title: string, content: string, timestamp: string }
+type NotifyProp = { title: string, content: string, timestamp: string, read: boolean}
 
 const Notify: React.FC<{ notify: NotifyProp }> = ({ notify }) => { 
     return (
           <button className="b">
-            <div className="flex-box flex-box-column  not-align">
-                <span className="not-title">{notify.title}</span>
-                <div className=" flex-box flex-box-row not-align " style={{ width: '100%' }}>
-                    <span className="not-desc">{notify.content}...</span>
-                    <span className="not-time">{ '15 min'}</span>
+            <div className="flex-box not-align " style={{ }}>
+                <div className="flex-box flex-box-column " style={{ width: '80%'}}>
+                    <span style={{ textAlign: 'left' }} className={ notify.read ? 'not-title' : "not-title-bold"}>{notify.title}</span>
+                    <span className="not-desc" style={{}}>{notify.content.substring(0, 40)}...</span>
+                </div>
+                <div style={{  width: '20%', height: 70}}>
+                    <div className="max-size flex-box flex-box-column align-horizontal align-vertical" style={{ gap: '10px' }}>
+                        {
+                            !notify.read && 
+                                <div className="circle-read"></div>
+                        }
+                        <span className="not-time">{ '15 min'}</span>
+                    </div>
                 </div>
             </div>
         </button>
